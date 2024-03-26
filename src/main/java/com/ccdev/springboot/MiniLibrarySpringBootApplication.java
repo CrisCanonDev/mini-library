b0bbdafa57705664477586e0bd3aabc184216fcf
CrisCanonDev <criscanon21@gmail.com>
2024-03-26 16:43:19 +0200

    first commit

---
+++ src/main/java/com/ccdev/springboot/MiniLibrarySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ccdev.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLibrarySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniLibrarySpringBootApplication.class, args);
+	}
+
+}
